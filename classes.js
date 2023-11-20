57abae8961f9388fc1b710b12c3b72915e72d3aa
Girleide de Araujo <gas27@aluno.ifal.edu.br>
2023-11-20 19:13:50 -0300

    ...

---
+++ classes.js
@@ -0,0 +1,10 @@
+class Palavra{
+    constructor(palavra){
+        this.palavra = palavra
+    }
+    consultarLetras(letra){
+        if (this.palavra.include(letra)) {
+            
+        }
+    }
+}
