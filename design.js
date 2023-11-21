0a4f6dac54aeffdd2306be2b69ec770c3dc40748
MelrickDavison <115827008+MelrickDavison@users.noreply.github.com>
2023-11-20 21:53:00 -0300

    Update

--- design.js
+++ design.js
@@ -1,9 +1,9 @@
 function desenhar(arrayWords, tag){
-
+for (let i = 0; i < arrayWords.length; i++) {
     tag.innerHTML +=
     `<div class='cubo'> 
-    <input type="text" name="" id="">
+    <input type="text" name="" id=${"inptTentativa" + i.toString()}>
  </div>
-
-`;
+        `;
+    }
 }
